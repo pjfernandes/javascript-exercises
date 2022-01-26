47a299306bd5793036aceee1f5fd52c3679c857b
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-26 18:29:56 -0300

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  return (5/9) * (temp - 32);
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return (9/5) * temp + 32;
 };
 
 // Do not edit below this line
