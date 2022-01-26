5f8cbbd58e2cf7de0d3f3938aa16fec8737ba448
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-26 17:57:13 -0300

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  let array = [];
+  for (let i = 0; i < num; i++) {
+    array[i] = string;
+  };
+  return array.join("");
 };
 
 // Do not edit below this line
