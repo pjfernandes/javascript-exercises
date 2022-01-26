1d1f7f12d828d599ba3ef54b46a9020b4c0b7c30
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-26 18:16:53 -0300

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+  let items = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      continue;
+    } else {
+      items.push(array[i]);
+    }
+  }
+  return items;
 };
 
 // Do not edit below this line
