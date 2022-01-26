63ace8329014f6549172d460265fcd58b95731ea
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-26 18:25:34 -0300

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+  let array = [];
+  let j = 1;
+  for (let i = 0; i < max; i++) {
+
+    array[i] = j;
+    j++;
+  };
+
+  let sum = 0;
+  array.forEach(element => {
+    sum = sum + element;
+  });
+  return sum;
 };
 
 // Do not edit below this line
