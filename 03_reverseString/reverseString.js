d37102e8e8b805fa6c0a10260b1acfa99e00e48e
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-26 18:06:23 -0300

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+  let array = [];
+  str_array = str.split("");
+  str_array.forEach((element) => {
+    array.unshift(element);
+  });
+  return array.join("");
 };
 
 // Do not edit below this line
