efaba10ebfff57b5d1ea346a42d7cc46c3dfa3b3
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-28 23:48:12 -0300

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const array = string.toLowerCase().replace(" ", "").replace(/[^\w\s]/gi, "").split("");
+  return array == array.reverse() ? true : false;
 };
 
 // Do not edit below this line
