8a45cbf0ad4f546bafb48236ec5375c7f75d251b
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-29 13:56:27 -0300

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+  let oldest;
+  let max = 0;
+
+  array.forEach(element => {
+    const life = element.yearOfDeath - element.yearOfBirth;
+    if (life > max) {
+      max = life;
+      oldest = element;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
