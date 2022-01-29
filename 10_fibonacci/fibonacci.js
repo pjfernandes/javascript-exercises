31728fb159b0777533eb2c2802a3102e6f7bf15b
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-28 23:54:55 -0300

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 2) {
+    return n;
+  } else {
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
 };
 
 // Do not edit below this line
