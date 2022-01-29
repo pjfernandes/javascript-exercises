62c517309ce73c3de41eed120318b0c0ebcae0f3
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-29 00:02:11 -0300

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  const titles = [];
+  array.forEach(element => {
+    titles.push(element.title);
+  });
+  return titles;
 };
 
 // Do not edit below this line
