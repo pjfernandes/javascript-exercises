5b30360d3f5f9571403fe5a7fc6b853a0959a7e4
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-28 23:24:33 -0300

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,5 +1,5 @@
 const add = function(a, b) {
-  return a + b;
+  return a+b;
 };
 
 const subtract = function(a, b) {
@@ -10,12 +10,16 @@ const subtract = function(a, b) {
 const sum = function(array) {
   const sum = array.reduce((total, element) => {
     return total + element;
-  }, undefined);
+  }, 0);
   return sum;
 };
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(array) {
+  let prod = 1;
+  array.forEach(element => {
+    prod = prod * element;
+  });
+  return prod;
 };
 
 const power = function(a, b) {
@@ -23,8 +27,12 @@ const power = function(a, b) {
 
 };
 
-const factorial = function() {
-
+const factorial = function(num) {
+  if (num === 0 || num == 1) {
+    return 1;
+  } else {
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
